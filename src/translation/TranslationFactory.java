6c8af3150975dd76caffda1126269807969e542f
luokai <kaiv2@outlook.com>
2019-01-10 10:42:18 +0800

    修复null异常

--- src/translation/TranslationFactory.java
+++ src/translation/TranslationFactory.java
@@ -1,37 +1,44 @@
 package translation;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.HashMap;
+import java.util.Map;
 
 import translation.baidu.BaiduTranslation;
 import translation.youdao.YouDaoTranslation;
 
 public class TranslationFactory {
 
-	private static final ConcurrentHashMap<Translation.API, Translation> map = new ConcurrentHashMap<Translation.API, Translation>();
+	private static final Map<Translation.API, Translation> T_MAP = new HashMap<Translation.API, Translation>();
 
 	public static Translation newIns(Translation.API t) {
 		return getOrNew(t);
 	}
 
 	private static Translation getOrNew(Translation.API t) {
-		Translation translation = map.get(t);
+		Translation translation = T_MAP.get(t);
 		if (translation != null) {
 			return translation;
 		} else {
-			if (map.containsKey(t)) {
-				return map.get(t);
-			}
-
-			switch (t) {
-			case Y:
-				return map.put(t, new YouDaoTranslation());
-			case B:
-				return map.put(t, new BaiduTranslation());
-			case G:
-				throw new RuntimeException("暂时不支持 google 翻译");
-			default:
-				throw new RuntimeException("没有选择第三方翻译API");
+			synchronized (t) {
+				if (T_MAP.containsKey(t)) {
+					return T_MAP.get(t);
+				}
+				
+				switch (t) {
+				case Y:
+					translation =  new YouDaoTranslation();
+					break;
+				case B:
+					translation = new BaiduTranslation();
+					break;
+				case G:
+					throw new RuntimeException("暂时不支持 google 翻译");
+				default:
+					throw new RuntimeException("没有选择第三方翻译API");
+				}
+				T_MAP.put(t, translation);
 			}
 		}
+		return translation;
 	}
 }
